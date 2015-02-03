2286b2884ec6d8cd3ccb09f7013da91865fb5980
Maxwell Mears & rilkevb <pair+imears.rilkevb@devbootcamp.com>
2015-02-03 12:10:48 -0800

    Release 2 complete

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,10 +1,18 @@
 class QuestionsController < ApplicationController
+  # before do
+  #   :find_question
+  # end
+
+  # def find_question
+  #   @question = Question.find(params[:id])
+  # end
+
   def index
     @questions = Question.all
   end
 
   def create
-    @question = Question.new(params[:question])
+    @question = Question.new(question_params)
 
     @question.save
     redirect_to @question
@@ -12,5 +20,32 @@ class QuestionsController < ApplicationController
 
   def show
     @question = Question.find(params[:id])
+  end
+
+  def edit
+    @question = Question.find(params[:id])
+  end
+
+  def update
+    @question = Question.find(params[:id])
+
+    if @question.update(question_params)
+      redirect_to @question
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    p "Made it in the destroy route"
+    @question = Question.find(question_params)
+    @question.destroy
+    redirect_to questions_path
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title, :content, :id)
   end
 end
